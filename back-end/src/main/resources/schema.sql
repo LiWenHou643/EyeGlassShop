c2425044333084b9c6194fbff35d233fc72fe019
le-van-hau <quannhan6969@gmail.com>
2024-08-14 18:32:21 +0700

    upload database file

---
+++ back-end/src/main/resources/schema.sql
@@ -0,0 +1,154 @@
+DROP SCHEMA IF EXISTS `EyeGlassShop`;
+CREATE SCHEMA `EyeGlassShop`;
+USE EyeGlassShop;
+
+CREATE TABLE `roles` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(20)
+);
+
+CREATE TABLE `customer` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `fullname` varchar(50) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `phone_number` varchar(20),
+  `address` varchar(200),
+  `password` varchar(200) NOT NULL,
+  `role_id` int NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE `tokens` (
+  `customer_id` int,
+  `token` varchar(32),
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`, `token`)
+);
+
+CREATE TABLE `category` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(100),
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE `sub_category` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `parent_id` int,
+  `name` varchar(100),
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE `product` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `category_id` int,
+  `title` varchar(250),
+  `price` int,
+  `discount` int,
+  `thumbnail` varchar(500),
+  `description` longtext,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE `galery` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `product_id` int,
+  `thumbnail` varchar(500),
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE `cart` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `customer_id` int,
+  `total` int,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE `cart_item` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `cart_id` int,
+  `product_id` int,
+  `quantiy` int,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE `payment_details` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `amount` int,
+  `provider` int,
+  `status` int,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE `orders_details` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `customer_id` int,
+  `payment_id` int,
+  `total` int,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+CREATE TABLE `order_item` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `order_id` int,
+  `product_id` int,
+  `price` int,
+  `quantity` int,
+  `created_at` TIMESTAMP NOT NULL,
+  `created_by` varchar(50) NOT NULL,
+  `updated_at` TIMESTAMP DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+);
+
+ALTER TABLE `customer` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+
+ALTER TABLE `sub_category` ADD FOREIGN KEY (`parent_id`) REFERENCES `category` (`id`);
+
+ALTER TABLE `galery` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+ALTER TABLE `tokens` ADD FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+ALTER TABLE `orders_details` ADD FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+ALTER TABLE `cart` ADD FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`);
+
+ALTER TABLE `product` ADD FOREIGN KEY (`category_id`) REFERENCES `sub_category` (`id`);
+
+ALTER TABLE `cart_item` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+ALTER TABLE `cart_item` ADD FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`);
+
+ALTER TABLE `order_item` ADD FOREIGN KEY (`order_id`) REFERENCES `orders_details` (`id`);
+
+ALTER TABLE `order_item` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+ALTER TABLE `orders_details` ADD FOREIGN KEY (`payment_id`) REFERENCES `payment_details` (`id`);
