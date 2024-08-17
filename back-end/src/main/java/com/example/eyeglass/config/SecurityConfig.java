1b0923381d9394cbe8167de70bca7610d9869068
LiWenHou <quannhan6969@gmail.com>
2024-08-17 09:36:43 +0700

    fix some bug, update prod profile

--- back-end/src/main/java/com/example/eyeglass/config/SecurityConfig.java
+++ back-end/src/main/java/com/example/eyeglass/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,20 +17,21 @@ import org.springframework.security.web.authentication.password.HaveIBeenPwnedRe
 
 @Configuration
 @Profile("!prod")
+@EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     SecurityFilterChain defaltSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession"))
-                .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure())
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/users/**").hasRole("USER")
-                        .requestMatchers("/api/**").permitAll()
-                        .requestMatchers("/test", "/myAccount").authenticated()
-                        .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession").permitAll()
-                );
+        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession"))
+            .requiresChannel(rcc -> rcc.anyRequest().requiresInsecure())
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                    .requestMatchers("/api/users/**").hasRole("USER")
+                    .requestMatchers("/api/**").permitAll()
+                    .requestMatchers("/test", "/myAccount").authenticated()
+                    .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession").permitAll()
+            );
 
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
--- back-end/src/main/java/com/example/eyeglass/config/SecurityProdConfig.java
+++ back-end/src/main/java/com/example/eyeglass/config/SecurityProdConfig.java
@@ -1,50 +1,54 @@
-//package com.example.eyeglass.config;
-//
-////import com.example.eyeglass.exception.CustomAccessDeniedHandler;
-////import com.example.eyeglass.exception.CustomBasicAuthenticationEntryPoint;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.security.authentication.password.CompromisedPasswordChecker;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-//
-//@Configuration
-//@Profile("prod")
-//@EnableWebSecurity
-//public class SecurityProdConfig {
-//
-//    @Bean
-//    SecurityFilterChain defaltSecurityFilterChain(HttpSecurity http) throws Exception {
-//        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession"))
-//            .csrf(AbstractHttpConfigurer::disable)
-//            .authorizeHttpRequests(authorize ->
-//                    authorize.requestMatchers("/api/admin/**").hasRole("ADMIN")
-//                             .requestMatchers("/api/users/**", "/invalidSession").permitAll()
-//                             .requestMatchers("/test").authenticated()
-//                             .anyRequest().authenticated()
-//            );
-//
-//        http.formLogin(Customizer.withDefaults());
-////        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
-////        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public CompromisedPasswordChecker compromisedPasswordChecker() {
-//        return new HaveIBeenPwnedRestApiPasswordChecker();
-//    }
-//}
+package com.example.eyeglass.config;
+
+//import com.example.eyeglass.exception.CustomAccessDeniedHandler;
+//import com.example.eyeglass.exception.CustomBasicAuthenticationEntryPoint;
+import com.example.eyeglass.exception.CustomAccessDeniedHandler;
+import com.example.eyeglass.exception.CustomBasicAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+@Configuration
+@Profile("prod")
+@EnableWebSecurity
+public class SecurityProdConfig {
+
+    @Bean
+    SecurityFilterChain defaltSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession"))
+            .requiresChannel(rcc -> rcc.anyRequest().requiresSecure())
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                    .requestMatchers("/api/users/**").hasRole("USER")
+                    .requestMatchers("/api/**").permitAll()
+                    .requestMatchers("/test", "/myAccount").authenticated()
+                    .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession").permitAll()
+            );
+
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}
