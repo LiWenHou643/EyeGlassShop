3b965f2fbba7feca3750f09099fd9c2d2193cfb7
LiWenHou <quannhan6969@gmail.com>
2024-08-29 19:17:04 +0700

    fix BE bug

--- back-end/src/main/java/com/example/eyeglass/EyeGlassApplication.java
+++ back-end/src/main/java/com/example/eyeglass/EyeGlassApplication.java
@@ -1,10 +1,14 @@
 package com.example.eyeglass;
 
 import com.example.eyeglass.config.RSAKeyRecord;
+import lombok.NonNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@SpringBootApplication
 @SpringBootApplication
@@ -14,5 +18,19 @@ public class EyeGlassApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EyeGlassApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:3000")
+                        .allowCredentials(true).maxAge(3600)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With")
+                        .exposedHeaders("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With");
+            }
+        };
     }
 }
