031a92394331abb5d22eb5990883a18ad046d474
LiWenHou <quannhan6969@gmail.com>
2024-08-31 18:48:07 +0700

    update sql schema

--- back-end/src/main/resources/schema.sql
+++ back-end/src/main/resources/schema.sql
@@ -14,11 +14,7 @@ CREATE TABLE `person` (
   `phone_number` varchar(20),
   `address` varchar(200),
   `password` varchar(200) NOT NULL,
-  `role_id` int NOT NULL,
-  `created_at` TIMESTAMP NOT NULL,
-  `created_by` varchar(50) NOT NULL,
-  `updated_at` TIMESTAMP DEFAULT NULL,
-  `updated_by` varchar(50) DEFAULT NULL
+  `role_id` int NOT NULL
 );
 
 CREATE TABLE `refresh_tokens` (
@@ -30,21 +26,7 @@ CREATE TABLE `refresh_tokens` (
 
 CREATE TABLE `category` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(100),
-  `created_at` TIMESTAMP NOT NULL,
-  `created_by` varchar(50) NOT NULL,
-  `updated_at` TIMESTAMP DEFAULT NULL,
-  `updated_by` varchar(50) DEFAULT NULL
-);
-
-CREATE TABLE `sub_category` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `parent_id` int,
-  `name` varchar(100),
-  `created_at` TIMESTAMP NOT NULL,
-  `created_by` varchar(50) NOT NULL,
-  `updated_at` TIMESTAMP DEFAULT NULL,
-  `updated_by` varchar(50) DEFAULT NULL
+  `name` varchar(100)
 );
 
 CREATE TABLE `product` (
@@ -54,64 +36,40 @@ CREATE TABLE `product` (
   `price` int,
   `discount` int,
   `thumbnail` varchar(500),
-  `description` longtext,
-  `created_at` TIMESTAMP NOT NULL,
-  `created_by` varchar(50) NOT NULL,
-  `updated_at` TIMESTAMP DEFAULT NULL,
-  `updated_by` varchar(50) DEFAULT NULL
+  `description` longtext
 );
 
 CREATE TABLE `galery` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `product_id` int,
-  `thumbnail` varchar(500),
-  `created_at` TIMESTAMP NOT NULL,
-  `created_by` varchar(50) NOT NULL,
-  `updated_at` TIMESTAMP DEFAULT NULL,
-  `updated_by` varchar(50) DEFAULT NULL
+  `thumbnail` varchar(500)
 );
 
 CREATE TABLE `cart` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `person_id` int,
-  `total` int,
-  `created_at` TIMESTAMP NOT NULL,
-  `created_by` varchar(50) NOT NULL,
-  `updated_at` TIMESTAMP DEFAULT NULL,
-  `updated_by` varchar(50) DEFAULT NULL
+  `total` int
 );
 
 CREATE TABLE `cart_item` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `cart_id` int,
   `product_id` int,
-  `quantiy` int,
-  `created_at` TIMESTAMP NOT NULL,
-  `created_by` varchar(50) NOT NULL,
-  `updated_at` TIMESTAMP DEFAULT NULL,
-  `updated_by` varchar(50) DEFAULT NULL
+  `quantiy` int
 );
 
 CREATE TABLE `payment_details` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `amount` int,
   `provider` int,
-  `status` int,
-  `created_at` TIMESTAMP NOT NULL,
-  `created_by` varchar(50) NOT NULL,
-  `updated_at` TIMESTAMP DEFAULT NULL,
-  `updated_by` varchar(50) DEFAULT NULL
+  `status` int
 );
 
 CREATE TABLE `orders_details` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `person_id` int,
   `payment_id` int,
-  `total` int,
-  `created_at` TIMESTAMP NOT NULL,
-  `created_by` varchar(50) NOT NULL,
-  `updated_at` TIMESTAMP DEFAULT NULL,
-  `updated_by` varchar(50) DEFAULT NULL
+  `total` int
 );
 
 CREATE TABLE `order_item` (
@@ -119,18 +77,12 @@ CREATE TABLE `order_item` (
   `order_id` int,
   `product_id` int,
   `price` int,
-  `quantity` int,
-  `created_at` TIMESTAMP NOT NULL,
-  `created_by` varchar(50) NOT NULL,
-  `updated_at` TIMESTAMP DEFAULT NULL,
-  `updated_by` varchar(50) DEFAULT NULL
+  `quantity` int
 );
 
 ALTER TABLE `person` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
 
 ALTER TABLE `refresh_tokens` ADD FOREIGN KEY (`person_id`) REFERENCES `person` (`id`);
-
-ALTER TABLE `sub_category` ADD FOREIGN KEY (`parent_id`) REFERENCES `category` (`id`);
 
 ALTER TABLE `galery` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 
@@ -138,7 +90,7 @@ ALTER TABLE `orders_details` ADD FOREIGN KEY (`person_id`) REFERENCES `person` (
 
 ALTER TABLE `cart` ADD FOREIGN KEY (`person_id`) REFERENCES `person` (`id`);
 
-ALTER TABLE `product` ADD FOREIGN KEY (`category_id`) REFERENCES `sub_category` (`id`);
+ALTER TABLE `product` ADD FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
 
 ALTER TABLE `cart_item` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 
